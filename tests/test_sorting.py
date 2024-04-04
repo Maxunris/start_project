fbbbd94497b9ae3beb7ab8ead2c2c4d06ae7518d
Maxim Rusin <Maxunris@gmail.com>
2024-04-04 21:48:12 +0300

    add 5 test

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -3,6 +3,9 @@ from selene import browser, have
 
 def test_sorting():
     browser.open('https://start.ru/')
-    browser.element('[data-testid="movies_button"]').click()
-    browser.element('.Catalog_catalog__Gjv4a').should(have.text('Фильмы - смотреть онлайн'))
+    browser.element('[data-testid="animation_button"]').click()
+    browser.element('[href="/animation/nurseryrhymes"]').click()
+    browser.element('[alt="sort"]').click()
+    browser.all('.CatalogSelect_select__item__Ea2Dr').element_by(have.exact_text('По рейтингу')).click()
+    browser.element('#first-item').should(have.text('Малышарики'))
 
