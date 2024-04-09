7648f627c15f40b039292d1b7a36f2bea6408770
Maxim Rusin <Maxunris@gmail.com>
2024-04-09 17:26:26 +0300

    add time sleep in kids test

--- tests/test_kids_mod.py
+++ tests/test_kids_mod.py
@@ -1,3 +1,5 @@
+import time
+
 from selene import browser, have
 import allure
 
@@ -10,4 +12,5 @@ def test_kids_mod():
         browser.element('.HeaderMenu_header-menu__wrapper___TgEa').click()
         browser.element('[data-testid="profile_kids"]').click()
     with allure.step('Checking text'):
+        time.sleep(5)
         browser.element('.VideoUnit_vline__header__title__link__title__Nuqlc').should(have.text('Новинки для детей'))
