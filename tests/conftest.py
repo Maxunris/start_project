71d2db14c431ff80c285cb74a1b1a0e64425bf1a
Maxim Rusin <Maxunris@gmail.com>
2024-04-10 17:28:15 +0300

    add gif

--- tests/conftest.py
+++ tests/conftest.py
@@ -73,7 +73,7 @@ def browser_manage():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = "eager"
     browser.config.driver_options = driver_options
-    browser.config.base_url = 'https://tochka.com'
+    browser.config.base_url = 'https://start.ru/'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
