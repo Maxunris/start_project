171c6acca59e580a431ba7669782156ae1358750
Maxim Rusin <Maxunris@gmail.com>
2024-04-11 17:54:01 +0300

    add pages for test_button_films.py

---
+++ pages/button_films_page.py
@@ -0,0 +1,15 @@
+from selene import browser, have, be
+import allure
+
+class MainPage:
+    def open(self):
+        with allure.step('Open site'):
+            browser.open("")
+
+    def click_films_button(self):
+        with allure.step('Films button clickable'):
+            browser.element('[data-testid="movies_button"]').click()
+
+    def check_films_text(self):
+        with allure.step('Checking text'):
+            browser.element('.Catalog_catalog__Gjv4a').should(have.text('Фильмы - смотреть онлайн'))
