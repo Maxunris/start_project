212f3307b8fe464cbdad5b075ca545e774d58c08
Maxim Rusin <Maxunris@gmail.com>
2024-04-12 19:14:05 +0300

    add allure for api

--- tests/API/test_trailer.py
+++ tests/API/test_trailer.py
@@ -1,8 +1,11 @@
+import allure
 import requests
 from jsonschema import validate
 from start_project.shemas.trailer_shemas import trailer
 
-def test_movie_api():
+
+@allure.title("Checking movie data")
+def test_trailer_api():
     BASE_URL = 'https://api.start.ru'
     endpoint = "/web/watch/vyzov"
     params = {"apikey": "a20b12b279f744f2b3c7b5c5400c4eb5",
@@ -11,15 +14,17 @@ def test_movie_api():
               }
 
     response = requests.get(BASE_URL + endpoint, params=params)
+    with allure.step('Status code=200'):
+        assert response.status_code == 200
+    with allure.step('title = Вызов'):
+        assert response.json()["title"] == "Вызов"
+    with allure.step('description correct'):
+        assert response.json()[
+                   "description"] == "Член экипажа МКС получает травму в открытом космосе. Помочь ему может только срочная операция, но провести ее в невесомости — дело из области фантастики. Сложнейшую задачу предстоит выполнить торакальному хирургу Жене. И у нее всего месяц на подготовку, но помешать героине могут не только перегрузки и отсутствие притяжения…Чем же закончится уникальная миссия?"
 
-    assert response.status_code == 200
-    assert response.json()["title"] == "Вызов"
-    assert response.json()[
-               "description"] == "Член экипажа МКС получает травму в открытом космосе. Помочь ему может только срочная операция, но провести ее в невесомости — дело из области фантастики. Сложнейшую задачу предстоит выполнить торакальному хирургу Жене. И у нее всего месяц на подготовку, но помешать героине могут не только перегрузки и отсутствие притяжения…Чем же закончится уникальная миссия?"
 
-
-
-def shema_validation():
+@allure.title("Checking the compliance of the API response with the scheme")
+def test_shema_validation():
     url = "https://api.start.ru"
     endpoint = "/web/watch/vyzov"
     params = {"apikey": "a20b12b279f744f2b3c7b5c5400c4eb5",
@@ -28,4 +33,5 @@ def shema_validation():
               }
 
     response = requests.get(url + endpoint, params=params)
-    validate(response.json(), trailer)
+    with allure.step('Shema is validate'):
+        validate(response.json(), trailer)
